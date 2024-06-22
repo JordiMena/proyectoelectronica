be992b38dfbf1258ec9c3921742ae2b16721bc5e
JordiMena <jjyordy78@gmail.com>
2024-06-22 19:56:44 -0400

    Add files via upload

---
+++ ControlDeInventario.Domain/Categoria.cs
@@ -0,0 +1,12 @@
+namespace Proyecto.Domain.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        // Relación con Productos
+        public ICollection<Producto> Productos { get; set; }
+    }
+}
